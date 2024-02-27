dbd90fa9984d0091d998bbe05d68748006c9a8a3
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-02-27 15:56:17 -0300

    New feature in header + new things

--- Portfolio/Photographer site/scripts/scroll.js
+++ Portfolio/Photographer site/scripts/scroll.js
@@ -1,9 +1,14 @@
 document.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
     header.classList.add("scrolled");
+    headerImg.src = "img/logo.png";
+
   } else {
     header.classList.remove("scrolled");
+    headerImg.src = "../img/logo-white.png";
+
   }
 })
 
 const header = document.querySelector("header");
+const headerImg = document.querySelector("#headerLogo");
