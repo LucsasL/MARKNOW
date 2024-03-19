173e445c13db388cc467d835fa7c5dce8616d31f
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-19 12:29:18 -0300

    Tired of this shit

--- Portfolio/Marketing agency/scripts/palsMessage.js
+++ Portfolio/Marketing agency/scripts/palsMessage.js
@@ -1,6 +1,17 @@
-const msgCont = document.querySelector(".testimTxt");
-const arrowBut = document
-  .querySelectorAll(".arrow")
-  .addEventListener("click", () => {
-    msgCont.style.left = "-100%";
-  });
+const msgCont = document.querySelector(".testimTxt p.center");
+const arrowBut = document.querySelectorAll(".arrow");
+const msgs = [0, 1, 2, 3];
+let msgShown = 0;
+
+arrowBut[0].addEventListener("click", () => {
+  msgCont.classList.add(".testimMsgRight");
+  
+})
+
+arrowBut[1].addEventListener("click", () => {
+  msgCont.classList.add(".testimMsgLeft");
+});
+
+if (msgShown > 4) {
+  msgShown -= msgShown;
+}
