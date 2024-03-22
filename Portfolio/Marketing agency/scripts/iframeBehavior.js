6777262af7c4f25656bd88bc37b6b2e7042c8ceb
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-03-22 07:58:35 -0300

    Iframe JavaScript setted

--- Portfolio/Marketing agency/scripts/iframeBehavior.js
+++ Portfolio/Marketing agency/scripts/iframeBehavior.js
@@ -1,7 +1,19 @@
 const servButton = document.querySelectorAll("section#job .prim-button");
-const iframeCont = document.querySelectorAll("body");
+const iframeCont = document.querySelector("body");
 const iframe = document.createElement("iframe");
 
-document.addEventListener("click", () => {
-  iframe.appendChild("iframe");
-})
+iframe.style.position = "fixed";
+iframe.style.top = "0";
+iframe.style.left = "0";
+iframe.style.zIndex = "1";
+
+iframe.style.background = "#EBBAB9";
+iframe.width = "100%";
+iframe.height = "100%";
+iframe.name = "servTour";
+
+for (let i = 0; i < 3; i++) {
+  servButton[i].addEventListener("click", () => {
+    iframeCont.appendChild(iframe);
+  })
+}
