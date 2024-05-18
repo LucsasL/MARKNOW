4948abf16d1e25acb5546b858759f98992c0b654
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-18 16:10:42 -0300

    Architecture created + Header's done

--- Portfolio/marknow/src/App.js
+++ Portfolio/marknow/src/App.js
@@ -1,7 +1,10 @@
+import "./styles/index.css";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-    
+      <Header />
     </>
   );
 }
---
+++ Portfolio/marknow/src/components/Header.jsx
@@ -0,0 +1,38 @@
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div>
+          <div>
+            <h1>
+              MARKNOW
+            </h1>
+          </div>
+
+          <div>
+            <ul>
+              <li>
+                <a href="#intro">HOME</a>
+              </li>
+
+              <li>
+                <a href="#about">ABOUT US</a>
+              </li>
+
+              <li>
+                 <a href="services">SERVICES</a>
+                </li>
+
+              <li>
+                <a href="contact">CONTACT</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header;
