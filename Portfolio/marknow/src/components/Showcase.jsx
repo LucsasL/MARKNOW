72fa8d37b95b84d87200956d722eb068f1be353b
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-21 17:19:18 -0300

    Starting showcase + Footer

---
+++ Portfolio/marknow/src/components/Showcase.jsx
@@ -0,0 +1,21 @@
+const Showcase = () => {
+  return (
+    <>
+      <section id="showcase">
+        <div className="window">
+          <div className="bar"></div>
+        </div>
+        
+        <div className="window">
+          <div className="bar"></div>
+        </div>
+
+        <div className="window">
+          <div className="bar"></div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Showcase
