c3c5900e276f572c87e0be331289aca310b8e0e0
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-24 17:21:38 -0300

    Starting to add a scroll feature in the pals section

---
+++ Portfolio/marknow/src/Utils/data.js
@@ -0,0 +1,22 @@
+export const palsInfo = [
+  {
+    "author": "Mark Zuninberg",
+    "quote": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus voluptas tempore dolorum voluptatem nihil delectus illo impedit modi, minima expedita, totam aliquid optio! Odio obcaecati similique alias minima dignissimos natus. Consectetur, enim dolor! Eum similique voluptatum maiores numquam ratione repudiandae!",
+    "ceo": "Faceduck"
+  },
+  {
+    "author": "Irineu Nusei",
+    "quote": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus voluptas tempore dolorum voluptatem nihil delectus illo impedit modi, minima expedita, totam aliquid optio! Odio obcaecati similique alias minima dignissimos natus. Consectetur, enim dolor! Eum similique voluptatum maiores numquam ratione repudiandae!",
+    "ceo": "Faceduck"
+  },
+  {
+    "author": "Poren quiant",
+    "quote": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus voluptas tempore dolorum voluptatem nihil delectus illo impedit modi, minima expedita, totam aliquid optio! Odio obcaecati similique alias minima dignissimos natus. Consectetur, enim dolor! Eum similique voluptatum maiores numquam ratione repudiandae!",
+    "ceo": "Faceduck"
+  },
+  {
+    "author": "Asi caka",
+    "quote": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus voluptas tempore dolorum voluptatem nihil delectus illo impedit modi, minima expedita, totam aliquid optio! Odio obcaecati similique alias minima dignissimos natus. Consectetur, enim dolor! Eum similique voluptatum maiores numquam ratione repudiandae!",
+    "ceo": "Faceduck"
+  }
+]
--- Portfolio/marknow/src/components/Pals.jsx
+++ Portfolio/marknow/src/components/Pals.jsx
@@ -1,31 +1,42 @@
+import { palsInfo } from "../Utils/data";
+
 const Pals = () => {
   return (
     <>
       <section id="pals">
         <div>
-          <h1>
-            OUR CLIENTS APPROVE
-          </h1>
+          <div className="arrowsNav">
+            <picture>&lt;</picture>
+          </div>
 
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus voluptas tempore dolorum voluptatem nihil delectus illo impedit modi, minima expedita, totam aliquid optio! Odio obcaecati similique alias minima dignissimos natus.
-            Consectetur, enim dolor! Eum similique voluptatum maiores numquam ratione repudiandae!
-          </p>
+          <div className="arrowsNav">
+            <picture>&gt;</picture>
+          </div>
 
-          <h2>
-            MARK ZUNINBERG <br />
-            <span>
-              CEO OF FACEDUCK
-            </span>
-          </h2>
+          <div className="msg">
+            {
+              palsInfo.map(({ author, quote, ceo }) => {
+                return (
+                  <div className="palsInfo">
+                    <h1>OUR CLIENTS APPROVE</h1>
+                    <p>
+                      {quote}
+                    </p>
+                    <h2>
+                      {author} <br />
+                      <span>CEO OF {ceo}</span>
+                    </h2>
+                  </div>
+                );
+              })
+            }
+          </div>
         </div>
 
-        <div className="palsComp">
-          
-        </div>
+        <div className="palsComp"></div>
       </section>
     </>
-  )
+  );
 }
 
 export default Pals;
