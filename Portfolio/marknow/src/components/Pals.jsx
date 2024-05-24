366d2c914d3dd78deb4b51ceeec683c5132d5cd6
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-24 18:30:06 -0300

    Almost done the scroll

--- Portfolio/marknow/src/components/Pals.jsx
+++ Portfolio/marknow/src/components/Pals.jsx
@@ -3,6 +3,8 @@ import { palsInfo } from "../Utils/data";
 
 const Pals = () => {
   const [position, setPosition] = useState(0);
+  const [color, setColor] = useState("$secondary");
+
   const scrollMsg = (arr) => {
     if (arr === "Prev" && position < 0) {
       setPosition(position + 100);
@@ -11,55 +13,62 @@ const Pals = () => {
       setPosition(position - 100);
     }
 
+    changeColor(position);
   }
 
+  const changeColor = (msg) => {
+    switch (msg) {
+      case -100:
+        setColor("$third");
+        return;
+
+      case -200:
+        setColor("$fourth");
+        return;
+
+      case -300:
+        setColor("$fifth");
+        return;
+
+      default:
+        return;
+    }
+  }
+ 
   return (
     <>
       <section id="pals">
         <div>
-          <div 
-            className="arrowsNav"
-            onClick={() => scrollMsg("Prev")}
-          >
+          <div className="arrowsNav" onClick={() => scrollMsg("Prev")}>
             <picture>&lt;</picture>
           </div>
 
-          <div 
-            className="arrowsNav"
-            onClick={() => scrollMsg("Next")}
-          >
+          <div className="arrowsNav" onClick={() => scrollMsg("Next")}>
             <picture>&gt;</picture>
           </div>
 
-          <div 
+          <div
             className="msg"
             style={{
-              left: (position + "%")
+              left: position + "%",
             }}
           >
-            {
-              palsInfo.map(({ author, quote, ceo }) => {
-                return (
-                  <div 
-                    className="palsInfo"
-                    key={Math.floor(Math.random()) * 100}
-                  >
-                    <h1>OUR CLIENTS APPROVE</h1>
-                    <p>
-                      {quote}
-                    </p>
-                    <h2>
-                      {author} <br />
-                      <span>CEO OF {ceo}</span>
-                    </h2>
-                  </div>
-                );
-              })
-            }
+            {palsInfo.map(({ author, quote, ceo }) => {
+              return (
+                <div className="palsInfo" key={Math.floor(Math.random()) * 100}>
+                  <h1 style={{ color: color }}>OUR CLIENTS APPROVE</h1>
+                  <p>{quote}</p>
+                  <h2 style={{ color: color }}>
+                    {author} <br />
+                    <span>CEO OF {ceo}</span>
+                  </h2>
+                </div>
+              );
+            })}
           </div>
         </div>
 
-        <div className="palsComp"></div>
+        <div className="palsComp" style={{ background: color }}></div>
       </section>
     </>
   );
