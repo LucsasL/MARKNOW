d5f1cccd10122f5e973aab77d85e13f0ee514db3
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-29 15:39:38 -0300

    Adding the iframes

--- Portfolio/marknow/src/components/Services.jsx
+++ Portfolio/marknow/src/components/Services.jsx
@@ -4,57 +4,63 @@ const Services = () => {
   return (
     <>
       <section id="services">
-        <h1>
-          IT'S THE WAY WE DO OUR JOB
-        </h1>
+        <h1>IT'S THE WAY WE DO OUR JOB</h1>
 
         <div>
           <div className="services">
-            <h2>
-              MAKE IT HAPPEN
-            </h2>
+            <h2>MAKE IT HAPPEN</h2>
 
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita dolorum quae fuga perspiciatis, necessitatibus reiciendis hic ex praesentium architecto sed soluta sunt temporibus doloribus nulla minima eius? Perspiciatis, optio voluptatum.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita
+              dolorum quae fuga perspiciatis, necessitatibus reiciendis hic ex
+              praesentium architecto sed soluta sunt temporibus doloribus nulla
+              minima eius? Perspiciatis, optio voluptatum.
             </p>
 
             <button className="primary">
-              <a href="#">Learn More</a>
+              <a href="../iframes/build.html" target="servIframe">
+                Learn More
+              </a>
             </button>
           </div>
 
           <div className="services">
-            <h2>
-              GOOD SIGHT IS ALWAYS NEEDED
-            </h2>
+            <h2>GOOD SIGHT IS ALWAYS NEEDED</h2>
 
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita dolorum quae fuga perspiciatis, necessitatibus reiciendis hic ex praesentium architecto sed soluta sunt temporibus doloribus nulla minima eius? Perspiciatis, optio voluptatum.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita
+              dolorum quae fuga perspiciatis, necessitatibus reiciendis hic ex
+              praesentium architecto sed soluta sunt temporibus doloribus nulla
+              minima eius? Perspiciatis, optio voluptatum.
             </p>
 
             <button className="primary">
-              <a href="#">Learn More</a>
+              <a href="../iframes/persuade.html" target="servIframe">
+                Learn More
+              </a>
             </button>
           </div>
 
           <div className="services">
-            <h2>
-              IMPRESSION THAT CONVERTS
-            </h2>
+            <h2>IMPRESSION THAT CONVERTS</h2>
 
             <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita dolorum quae fuga perspiciatis, necessitatibus reiciendis hic ex praesentium architecto sed soluta sunt temporibus doloribus nulla minima eius? Perspiciatis, optio voluptatum.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita
+              dolorum quae fuga perspiciatis, necessitatibus reiciendis hic ex
+              praesentium architecto sed soluta sunt temporibus doloribus nulla
+              minima eius? Perspiciatis, optio voluptatum.
             </p>
 
             <button className="primary">
-              <a href="#">Learn More</a>
+              <a href="../iframes/convert.html" target="servIframe">
+                Learn More
+              </a>
             </button>
           </div>
-
         </div>
       </section>
     </>
-  )
+  );
 }
 
 export default Services;
