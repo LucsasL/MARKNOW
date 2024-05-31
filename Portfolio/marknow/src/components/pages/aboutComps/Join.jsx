d2601adf4c58b62f4a27e1863bdf5f0ba7ab0fbf
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-05-31 01:06:04 -0300

    More upgrades in the about's style

--- Portfolio/marknow/src/components/pages/aboutComps/Join.jsx
+++ Portfolio/marknow/src/components/pages/aboutComps/Join.jsx
@@ -1,20 +1,24 @@
 function Join() {
   return (
     <>
-      <section id="join" class="center-box">
-        <div class="cont">
-          <h1 class="title center">JOIN US</h1>
+      <section id="join">
+        <div>
+          <h1>JOIN US</h1>
 
-          <p class="center">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. In non
-            itaque repellat aliquid exercitationem debitis vero rem. Voluptas
-            iste tempora deleniti adipisci quos quaerat reiciendis culpa nam in,
-            sapiente laboriosam.
-          </p>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. In non
+              itaque repellat aliquid exercitationem debitis vero rem. Voluptas
+              iste tempora deleniti adipisci quos quaerat reiciendis culpa nam in,
+              sapiente laboriosam.
+            </p>
+          </div>
 
-          <button class="prim-button">
-            <a href="https://youtube.com">COME HERE</a>
-          </button>
+          <div>
+            <button class="prim-button">
+              <a href="https://youtube.com">COME HERE</a>
+            </button>
+          </div>
         </div>
       </section>
     </>
