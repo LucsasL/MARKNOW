f269f7046f6e86a5718a88f80144ae3cdeb4e0e2
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-01 03:33:02 -0300

    Little additions

--- Portfolio/marknow/src/components/Footer.jsx
+++ Portfolio/marknow/src/components/Footer.jsx
@@ -1,14 +1,17 @@
-import sectWaves from "../img/waves.svg";
+import { NavLink } from "react-router-dom";
 import { useState } from "react";
+
+// Images
+import sectWaves from "../img/waves.svg";
 
 const Footer = () => {
   const [newsletterEmail, setNewsletterEmail] = useState("");
-  
+
   return (
     <>
       <footer>
         <img src={sectWaves} alt="Waves" className="waves" />
-        
+
         <div>
           <div>
             <h1>This is MARKDOWN</h1>
@@ -22,7 +25,16 @@ const Footer = () => {
               value={newsletterEmail}
               onChange={(e) => setNewsletterEmail(e.target.value)}
             />
-            <input type="submit" value="→" />
+            <input
+              type="submit"
+              value="→"
+              onClick={() =>
+                alert(
+                  `This is a fictitious company and all of this site is a project to my portfolio. - Lucsas
+Project Link: https://github.com/lucsasl`
+                )
+              }
+            />
           </div>
 
           <div>
@@ -30,16 +42,16 @@ const Footer = () => {
 
             <ul>
               <li>
-                <a href="index.html">HOME</a>
+                <NavLink to="/">HOME</NavLink>
               </li>
               <li>
-                <a href="about.html">ABOUT US</a>
+                <NavLink to="/about">ABOUT US</NavLink>
               </li>
               <li>
-                <a href="#services">SERVICES</a>
+                <NavLink to="/">SERVICES</NavLink>
               </li>
               <li>
-                <a href="contact.html">CONTACT</a>
+                <NavLink to="/contact">CONTACT</NavLink>
               </li>
             </ul>
           </div>
@@ -49,13 +61,13 @@ const Footer = () => {
 
             <ul>
               <li>
-                <a href="https://instagram.com/">@LUCSAS.L</a>
+                <a href="https://www.instagram.com/lucsas.l/" target="_blank" rel="noopener noreferrer">@LUCSAS.L</a>
               </li>
               <li>
-                <a href="https://github.com/lucsasl">@LUCSASL</a>
+                <a href="https://github.com/lucsasl" target="_blank" rel="noopener noreferrer">@LUCSASL</a>
               </li>
               <li>
-                <a href="https://youtube.com">@LUCAS LIRA</a>
+                <a href="https://www.linkedin.com/in/lucsas/" target="_blank" rel="noopener noreferrer">@LUCAS LIRA</a>
               </li>
             </ul>
           </div>
--- Portfolio/marknow/src/components/pages/aboutComps/Top.jsx
+++ Portfolio/marknow/src/components/pages/aboutComps/Top.jsx
@@ -1,8 +1,37 @@
+import arrowPrev from "../../../icons/back-arrow.svg";
+import arrowNext from "../../../icons/front-arrow.svg";
+
 function Top() {
   return (
     <>
       <section id="top">
         <div>
+          <div
+            className="arrowsNav"
+            // onClick={() => scrollMsg("Prev")}
+            // onLoad={() => arrOpacity()}
+            // style={{ opacity: prevArrOpacity }}
+          >
+            <picture>
+              <img
+                src={arrowPrev}
+                alt="Previous Testimonial"
+                className="PrArrow"
+              />
+            </picture>
+          </div>
+
+          <div
+            className="arrowsNav"
+            // onClick={() => scrollMsg("Next")}
+            // onLoad={() => arrOpacity()}
+            // style={{ opacity: nextArrOpacity }}
+          >
+            <picture>
+              <img src={arrowNext} alt="Next Testimonial" className="NeArrow" />
+            </picture>
+          </div>
+
           <h1 className="bolder center title">
             Be in the top with the greatest
           </h1>
@@ -10,41 +39,29 @@ function Top() {
           <div className="profInfo">
             <div className="profDesc">
               <p>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis sapiente doloremque quia ducimus nostrum officiis aliquid sed nesciunt, nisi necessitatibus corrupti non, alias dignissimos magnam. Totam impedit magnam voluptatibus perferendis.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis
+                sapiente doloremque quia ducimus nostrum officiis aliquid sed
+                nesciunt, nisi necessitatibus corrupti non, alias dignissimos
+                magnam. Totam impedit magnam voluptatibus perferendis.
               </p>
 
               <div className="buttonDiv">
-                <button>
+                <button></button>
 
-                </button>
+                <button></button>
 
-                <button>
-                  
-                </button>
+                <button></button>
 
-                <button>
-                  
-                </button>
-
-                <button>
-                  
-                </button>
+                <button></button>
               </div>
             </div>
 
-            <div className="img-prof">
-              <picture>
-                {/* <source media="(min-width: )" srcset=""> */}
-                <figure>
-                  {/* <img src="../img/big-professional-round.jpg" alt="Be on top with our professionals"> */}
-                </figure>
-              </picture>
-            </div>
+            <div className="img-prof"></div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
 
 export default Top;
