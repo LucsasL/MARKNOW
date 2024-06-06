6af35858aa24be36d04cbcb56110b52996122b49
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-06-06 17:16:49 -0300

    Trying to deploy this thing in ages

---
+++ src/components/pages/indexComps/About.jsx
@@ -0,0 +1,39 @@
+const About = () => {
+  return (
+    <>
+      <section id="about">
+        <h1>
+          ALL AROUND THE WORLD
+        </h1>
+
+        <div>
+          <div>
+            <h2>
+              MORE THAN <br />
+              <span>500</span> <br />
+              CLIENTS
+            </h2>
+          </div>
+
+          <div>
+            <h2>
+              RETURNED <br />
+              <span>+5B</span> <br />
+              PROFIT
+            </h2>
+          </div>
+
+          <div>
+            <h2>
+              INVESTED <br />
+              <span>+1B</span> <br />
+              FUNDS
+            </h2>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default About;
