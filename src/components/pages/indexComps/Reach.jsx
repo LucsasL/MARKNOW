a0b277e0a639a3b7d85747e701530fb3e653d35a
Lucas Lira <129066213+LucsasL@users.noreply.github.com>
2024-10-29 13:45:46 -0300

    I have no idea how to fix this

--- src/components/pages/indexComps/Reach.jsx
+++ src/components/pages/indexComps/Reach.jsx
@@ -1,80 +1,97 @@
-import React, { useRef } from "react";
+// React Imports
+import React, { useRef, useEffect, createContext } from "react";
 
+// Three.js Imports
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/examples/jsm/Addons.js";
+import { useLoader } from "@react-three/fiber";
+
+// import { GLTFLoader } from "three/examples/jsm/Addons.js";
 
 const Reach = () => {
-  // Creating a Scene
-  const scene = new THREE.Scene();
-
-  // Main Model Variables
-  const aspectRatio = window.innerWidth / 1000;
-  const maxPixelRatio = Math.min(window.devicePixelRatio, 2);
-
   // Refs
   const canvas = useRef();
 
-  const gltfLoader = new GLTFLoader();
+  const CanvasRef = createContext();
 
-  function loadThree() {
-    const geo = new THREE.SphereGeometry(1, 32, 32);
-    const mat = new THREE.MeshBasicMaterial({ color: "blue" });
-    const mesh = new THREE.Mesh(geo, mat);
+  // Creating, Loading and Setting Three.js Environment 
+  useEffect(() => {
+    // Creating a Scene
+    const scene = new THREE.Scene();
 
-    // Texture
-    const cubeTextureLoader = new THREE.CubeTextureLoader();
-    cubeTextureLoader.setPath("../../../canvas/textures/cubeMap/");
+    // Main Model Variables
+    const aspectRatio = window.innerWidth / 1000;
+    const maxPixelRatio = Math.min(window.devicePixelRatio, 2);
 
-    scene.add(mesh);
+    const loadThree = () => {
+      const geo = new THREE.SphereGeometry(1, 32, 32);
+      const mat = new THREE.MeshBasicMaterial({ color: "blue" });
+      const mesh = new THREE.Mesh(geo, mat);
 
-    // Initializing a Light
-    const light = new THREE.AmbientLight("white", 1);
-    scene.add(light);
+      // Texture Loader
+      const textureLoader = new THREE.TextureLoader();
+      textureLoader.load("");
 
-    // Intitializing a Camera
-    const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.5, 2000);
+      // Cube Texture
+      const cubeTextureLoader = new THREE.CubeTextureLoader();
+      cubeTextureLoader.setPath("../../../../canvas/cubeMap/");
 
-    // Initializing a renderer
-    const renderer = new THREE.WebGLRenderer({
-      canvas: canvas.current,
-      antialias: true,
-    });
-    renderer.setSize(window.innerWidth, 1000);
-    renderer.setPixelRatio(maxPixelRatio);
+      scene.add(mesh);
 
-    // const bgCubeMap = cubeTextureLoader.load([
-    //   "px.png",
-    //   "nx.png",
-    //   "py.png",
-    //   "ny.png",
-    //   "pz.png",
-    //   "nz.png",
-    // ]);
+      // Initializing a Light
+      const light = new THREE.AmbientLight("white", 1);
+      scene.add(light);
 
-    // scene.background = bgCubeMap;
+      // Intitializing a Camera
+      const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.5, 2000);
+      camera.position.set(0, 0, 5);
 
-    // Initializing OrbitControls
-    const controls = new OrbitControls(camera, canvas.current);
-    controls.autoRotate = true;
-
-    // Resize Event Handler
-    window.addEventListener("resize", () => {
-      console.log("Screen resized.");
-      camera.aspect = window.innerWidth / 1000;
-      camera.updateProjectionMatrix();
+      // Initializing a renderer
+      const renderer = new THREE.WebGLRenderer({
+        canvas: canvas.current,
+        antialias: true,
+      });
       renderer.setSize(window.innerWidth, 1000);
-    });
+      renderer.setPixelRatio(maxPixelRatio);
 
-    // Creating a render loop
-    const renderloop = () => {
-      controls.update();
-      renderer.render(scene, camera);
-      window.requestAnimationFrame(renderloop);
-    };
+      const [bgCubeMap] = cubeTextureLoader.load([
+        "px.png",
+        "nx.png",
+        "py.png",
+        "ny.png",
+        "pz.png",
+        "nz.png",
+      ]);
 
-    renderloop();
-  }
+      scene.background = bgCubeMap;
+
+      // Initializing OrbitControls
+      const controls = new OrbitControls(camera, canvas.current);
+      controls.autoRotate = true;
+      controls.enableZoom = false;
+
+      // Resize Event Handler
+      window.addEventListener("resize", () => {
+        console.log("Screen resized.");
+        camera.aspect = window.innerWidth / 1000;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, 1000);
+      });
+
+      // Creating a render loop
+      const renderloop = () => {
+        controls.update();
+        renderer.render(scene, camera);
+        window.requestAnimationFrame(renderloop);
+      };
+
+      renderloop();
+    }
+
+    loadThree();
+  });
+
+  // const gltfLoader = new GLTFLoader();
 
   // gltfLoader.load("../../../canvas/earth.gltf", (gltfScene) => {
   //   let loadModel = gltfScene;
@@ -82,13 +99,13 @@ const Reach = () => {
   //   scene.add(gltfScene);
   // });
 
-  loadThree();
-
   return (
     <>
-      <section id="reach">
-        <canvas ref={canvas}></canvas>
-      </section>
+      <CanvasRef.Provider value={{ canvas }}>
+        <section id="reach">
+          <canvas ref={canvas}></canvas>
+        </section>
+      </CanvasRef.Provider>
     </>
   );
 };
